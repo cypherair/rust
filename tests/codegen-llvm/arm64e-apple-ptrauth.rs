c080e6a078e1cd62da853566c4db648a8004efdd
cypherair <262752927+cypherair@users.noreply.github.com>
2026-04-22 15:29:54 -0700

    arm64e: reject disabling default Apple ptrauth

--- tests/codegen-llvm/arm64e-apple-ptrauth.rs
+++ tests/codegen-llvm/arm64e-apple-ptrauth.rs
@@ -1,7 +1,11 @@
 //@ add-minicore
-//@ revisions: DARWIN IOS TVOS VISIONOS
+//@ revisions: DARWIN DARWIN_BTI DARWIN_GCS IOS TVOS VISIONOS
 //@ [DARWIN] compile-flags: --target arm64e-apple-darwin
 //@ [DARWIN] needs-llvm-components: aarch64
+//@ [DARWIN_BTI] compile-flags: --target arm64e-apple-darwin -Zbranch-protection=bti
+//@ [DARWIN_BTI] needs-llvm-components: aarch64
+//@ [DARWIN_GCS] compile-flags: --target arm64e-apple-darwin -Zbranch-protection=gcs
+//@ [DARWIN_GCS] needs-llvm-components: aarch64
 //@ [IOS] compile-flags: --target arm64e-apple-ios
 //@ [IOS] needs-llvm-components: aarch64
 //@ [TVOS] compile-flags: --target arm64e-apple-tvos
@@ -22,8 +26,9 @@ pub fn test() {}
 
 // CHECK: attributes [[ATTR]] = {{.*}} "ptrauth-auth-traps" "ptrauth-calls" "ptrauth-indirect-gotos" "ptrauth-returns"
 // CHECK-SAME: "target-features"="{{.*}}+pauth{{.*}}"
-// CHECK-NOT: "sign-return-address"
-// CHECK-NOT: "sign-return-address-key"
+// DARWIN_BTI: !{{[0-9]+}} = !{i32 8, !"branch-target-enforcement", i32 1}
+// DARWIN_GCS: !{{[0-9]+}} = !{i32 8, !"guarded-control-stack", i32 1}
+
 // CHECK: !{{[0-9]+}} = !{i32 6, !"ptrauth.abi-version", ![[OUTER:[0-9]+]]}
 // CHECK: ![[OUTER]] = !{![[PAYLOAD:[0-9]+]]}
 // CHECK: ![[PAYLOAD]] = !{i32 0, i1 false}
