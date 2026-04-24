9b94009e7e300eebc8ab3b6df9ee3021bb4d3171
cypherair <262752927+cypherair@users.noreply.github.com>
2026-04-23 18:19:50 -0700

    arm64e: emit clang-compatible ptrauth metadata

--- compiler/rustc_llvm/llvm-wrapper/RustWrapper.cpp
+++ compiler/rustc_llvm/llvm-wrapper/RustWrapper.cpp
@@ -1027,6 +1027,13 @@ extern "C" void LLVMRustAddModuleFlagString(
       MDString::get(unwrap(M)->getContext(), StringRef(Value, ValueLen)));
 }
 
+extern "C" void LLVMRustAddModuleFlagMetadata(
+    LLVMModuleRef M, LLVMRustModuleFlagMergeBehavior MergeBehavior,
+    const char *Name, size_t NameLen, LLVMMetadataRef Value) {
+  unwrap(M)->addModuleFlag(fromRust(MergeBehavior), StringRef(Name, NameLen),
+                           unwrap<Metadata>(Value));
+}
+
 extern "C" LLVMValueRef LLVMRustGetLastInstruction(LLVMBasicBlockRef BB) {
   auto Point = unwrap(BB)->rbegin();
   if (Point != unwrap(BB)->rend())
--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -1454,6 +1454,8 @@ symbols! {
         overflow_checks,
         overlapping_marker_traits,
         owned_box,
+        paca,
+        pacg,
         packed,
         packed_bundled_libs,
         panic,
