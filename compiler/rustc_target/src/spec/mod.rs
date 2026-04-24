1b3d8853425f0506a12b4d6cd685a76a78f5f51e
cypherair <262752927+cypherair@users.noreply.github.com>
2026-04-23 18:17:37 -0700

    target: add arm64e-apple-visionos

--- compiler/rustc_target/src/spec/mod.rs
+++ compiler/rustc_target/src/spec/mod.rs
@@ -1604,6 +1604,7 @@ supported_targets! {
 
     ("aarch64-apple-visionos", aarch64_apple_visionos),
     ("aarch64-apple-visionos-sim", aarch64_apple_visionos_sim),
+    ("arm64e-apple-visionos", arm64e_apple_visionos),
 
     ("armebv7r-none-eabi", armebv7r_none_eabi),
     ("armebv7r-none-eabihf", armebv7r_none_eabihf),
