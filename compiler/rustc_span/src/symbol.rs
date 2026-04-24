1a4c19c523b501d3461503067222b6820db17edc
cypherair <262752927+cypherair@users.noreply.github.com>
2026-04-23 18:19:50 -0700

    arm64e: emit clang-compatible ptrauth metadata

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -1441,6 +1441,8 @@ symbols! {
         overflow_checks,
         overlapping_marker_traits,
         owned_box,
+        paca,
+        pacg,
         packed,
         packed_bundled_libs,
         panic,
