cc959f9f7d62fd2ebeee50e7d18e677ee5309e64
cypherair <262752927+cypherair@users.noreply.github.com>
2026-04-23 21:46:49 -0700

    arm64e: authenticate function pointers used as data

--- compiler/rustc_codegen_ssa/src/traits/misc.rs
+++ compiler/rustc_codegen_ssa/src/traits/misc.rs
@@ -19,6 +19,9 @@ pub trait MiscCodegenMethods<'tcx>: BackendTypes {
     }
     fn get_fn(&self, instance: Instance<'tcx>) -> Self::Function;
     fn get_fn_addr(&self, instance: Instance<'tcx>) -> Self::Value;
+    fn get_fn_addr_for_data(&self, instance: Instance<'tcx>) -> Self::Value {
+        self.get_fn_addr(instance)
+    }
     fn eh_personality(&self) -> Self::Function;
     fn sess(&self) -> &Session;
     fn set_frame_pointer_type(&self, llfn: Self::Function);
